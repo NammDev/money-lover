ce5f11f3fa0698b4539246eb437fad842942cf9a
Nam <namdeveloper.ca@gmail.com>
2024-04-26 15:14:00 +0700

    clerk

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    const url = new URL(req.nextUrl.origin)
+
+    auth().protect({
+      unauthenticatedUrl: `${url.origin}/signin`,
+      unauthorizedUrl: `${url.origin}/dashboard/stores`,
+    })
+  }
+})
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
